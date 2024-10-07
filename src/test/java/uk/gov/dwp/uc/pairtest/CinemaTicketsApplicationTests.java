d49ffe2d7d94a4ab006629e3374cc9056427901f
Bolaji Adesina <adesina.bolaji@yahoo.com>
2024-10-07 21:17:13 +0100

    initial staging

---
+++ src/test/java/uk/gov/dwp/uc/pairtest/CinemaTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
